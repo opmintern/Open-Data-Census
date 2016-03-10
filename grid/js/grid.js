b43af9a0a60d37b5dfff6fbbf5a5093f185af5bf
Waldo Jaquith <waldo@jaquith.org>
2016-03-10 12:23:05 -0500

    Re-deploy

--- grid/js/grid.js
+++ grid/js/grid.js
@@ -36,13 +36,121 @@ var spinner = new Spinner(opts).spin(target);
 
  });
 
+function mapInfo(data, tabletop) {
+
+}
 
  var rawData = [];
 
  function showInfo(data, tabletop) {
+    
+     /* Stop our progress indicator, now that data is loaded. */
+     spinner.stop();
 
-    spinner.stop();
+     /* Put together data for the map. */
+     var state_abbreviations ={
+        "al": "Alabama",
+        "ar": "Arkansas",
+        "az": "Arizona",
+        "ak": "Alaska",
+        "ca": "California",
+        "co": "Colorado",
+        "ct": "Connecticut",
+        "de": "Delaware",
+        "fl": "Florida",
+        "ga": "Georgia",
+        "hi": "Hawaii",
+        "id": "Idaho",
+        "il": "Illinois",
+        "in": "Indiana",
+        "ia": "Iowa",
+        "ks": "Kansas",
+        "ky": "Kentucky",
+        "la": "Louisiana",
+        "me": "Maine",
+        "md": "Maryland",
+        "ma": "Massachusetts",
+        "ms": "Mississippi",
+        "mi": "Michigan",
+        "mn": "Minnesota",
+        "mo": "Missouri",
+        "mt": "Montana",
+        "ne": "Nebraska",
+        "nv": "Nevada",
+        "nh": "New Hampshire",
+        "nj": "New Jersey",
+        "nm": "New Mexico",
+        "ny": "New York",
+        "nc": "North Carolina",
+        "nd": "North Dakota",
+        "oh": "Ohio",
+        "ok": "Oklahoma",
+        "or": "Oregon",
+        "pa": "Pennsylvania",
+        "pr": "Puerto Rico",
+        "ri": "Rhode Island",
+        "sc": "South Carolina",
+        "sd": "South Dakota",
+        "tn": "Tennessee",
+        "tx": "Texas",
+        "ut": "Utah",
+        "vt": "Vermont",
+        "va": "Virginia",
+        "wa": "Washington",
+        "dc": "Washington DC",
+        "wv": "West Virginia",
+        "wi": "Wisconsin",
+        "wy": "Wyoming"
+     }
+     rawData = tabletop.sheets('Places').all();
+     var entity_scores = {};
+     for (var key in rawData){
+        name = rawData[key]['Name'];
+        for (var key2 in state_abbreviations){
+            if (state_abbreviations[key2] == name){
+                abbreviation = key2;
+            }
+        }
+        entity_scores[abbreviation] = rawData[key]['Score']
+     }
 
+     var score_colors = {
+        "20": "#f7fbff",
+        "30": "#deebf7",
+        "40": "#c6dbef",
+        "50": "#9ecae1",
+        "60": "#6baed6",
+        "70": "#4292c6",
+        "80": "#2171b5",
+        "90": "#084594",
+     }
+
+     var entity_colors = {};
+     for (var key in entity_scores){
+        score = Math.floor(entity_scores[key] / 10) * 10;
+        if (score < 20) score = 20;
+        entity_colors[key] = score_colors[score];
+     }
+
+     $('#vmap').vectorMap({
+       map: 'usa_en',
+       backgroundColor: null,
+       color: '#eee',
+       showTooltip: true,
+       showLabels: true,
+       selectedColor: null,
+       hoverColor: "#f66",
+       colors: entity_colors,
+       onRegionClick: function(event, code, region){
+            event.preventDefault();
+            window.location.href = "/datasets.html?state=" + region;
+       },
+       onResize: function (element, width, height) {
+            console.log('Map Size: ' +  width + 'x' +  height)
+       }
+     });
+
+     /* Now create the table of data. */
      var stateTemplate = Handlebars.compile($("#state-template").html());
 
      rawData = tabletop.sheets("Census Data").all()
@@ -51,7 +159,6 @@ var spinner = new Spinner(opts).spin(target);
          })
          .unique()
          .value();
-
 
      setupDatatypes(allTypes);
 
@@ -142,9 +249,6 @@ var spinner = new Spinner(opts).spin(target);
 
      $('[data-toggle="tooltip"]').tooltip()
  }
-
-
-
 
  function setupDatatypes(allTypes) {
      var datatypes = _.chain(allTypes).map(function(type) {
